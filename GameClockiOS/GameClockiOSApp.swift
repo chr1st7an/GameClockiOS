3a8ff6efbf28c617ec58002c682a35739440a55b
Christian Rodriguez <christian796rodriguez@gmail.com>
2023-02-16 16:49:46 -0500

    Initial Commit

---
+++ GameClockiOS/GameClockiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameClockiOSApp.swift
+//  GameClockiOS
+//
+//  Created by Christian Rodriguez on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GameClockiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
